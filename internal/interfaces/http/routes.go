55c646597130dd32636f4e6224db613f47f8a49a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-12-19 19:52:35 +0100

    refactor folders

--- internal/interfaces/http/routes.go
+++ internal/interfaces/http/routes.go
@@ -1,7 +1,15 @@
 package http
 
-import "net/http"
+import (
+	"net/http"
+	
+	usershttp "fin-flow-api/internal/users/interfaces/http"
+)
 
 func SetupRoutes(mux *http.ServeMux) {
+	// Health check
 	mux.HandleFunc("/health", HealthHandler)
+	
+	// User routes
+	usershttp.SetupRoutes(mux)
 }
---
+++ internal/users/interfaces/http/routes.go
@@ -0,0 +1,8 @@
+package http
+
+import "net/http"
+
+func SetupRoutes(mux *http.ServeMux) {
+	// User routes will be added here
+	// mux.HandleFunc("/users", ...)
+}
